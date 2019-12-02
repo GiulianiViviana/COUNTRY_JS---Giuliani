9072eb0c94cb9038ac90ad158cd3deb82978600b
GiulianiViviana <giuliani.viviana@iisgalvanimi.edu.it>
2019-12-02 10:20:21 +0000

    Fine esercizio 3.2

--- COUNTRY_JS/routes/countries.js
+++ COUNTRY_JS/routes/countries.js
@@ -5,5 +5,7 @@ var country = require('countryjs');
 router.get('/info/:state/', function(req, res, next){ //Rendiamo lo stato un parametro
  res.send(country.info(req.params.state));
 })
-
+router.get('/pages/:state/', function(req, res, next){
+ res.render('state', {state: country.info(req.params.state)}) ; //Rendiamo lo stato un parametro
+})
 module.exports = router;
